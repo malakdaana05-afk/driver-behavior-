030f7dba8e09baafd7b523af306a4ea6dfe9aaad
malakdaana05-afk <malakdaana05@gmail.com>
2026-05-15 20:06:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.write(
 @st.cache_resource
 def load_my_model():
 
-    return load_model("final_model.keras", compile=False)
+    return load_model("clean_model.h5", compile=False)
 model = load_my_model()
 
 # =========================
